ba0093df3e6cdece0e06df8275cc26edca760334
Michael Milewski <saramic@gmail.com>
2020-11-25 20:13:09 +1100

    place the react chart and change to column
    also:
      - fix the datetime by using number values
      - use turbolinks:load

--- app/javascript/packs/button_report.jsx
+++ app/javascript/packs/button_report.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Report from '../src/report';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('reports_data');
+document.addEventListener('turbolinks:load', () => {
+  const node = document.getElementById('button-report');
   const data = JSON.parse(node.getAttribute('data'));
   ReactDOM.render(
     <Report reports={data} />,
-    document.body.appendChild(document.createElement('div')),
+    document.getElementById('button-report'),
   );
 });
--- app/javascript/src/report.jsx
+++ app/javascript/src/report.jsx
@@ -5,7 +5,12 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 const Report = ({ reports }) => {
-  const data = { title: { text: 'Widget X' }, xAxis: { type: 'datetime' }, series: [{ data: reports }] };
+  const data = {
+    title: { text: 'Daily Count' },
+    xAxis: { type: 'datetime' },
+    yAxis: { title: { text: 'Count' } },
+    series: [{ type: 'column', data: reports }],
+  };
   return <HighchartsReact highcharts={Highcharts} options={data} />;
 };
 
