a0a50d81db03d7ae4a63031404d1ae87f25d7864
Michael Milewski <saramic@gmail.com>
2020-11-25 20:22:52 +1100

    quick hack to get raw report on a chart
    should have different name, reuse same pack tag and probably be a
    histogram

---
+++ app/javascript/packs/button_raw_report.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Report from '../src/report';
+
+document.addEventListener('turbolinks:load', () => {
+  const node = document.getElementById('button-raw-report');
+  const data = JSON.parse(node.getAttribute('data'));
+  ReactDOM.render(
+    <Report reports={data} />,
+    document.getElementById('button-raw-report'),
+  );
+});
