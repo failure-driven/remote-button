118e50a6ee3f979d0bac51b5464b0c47c62e264a
Michael Milewski <saramic@gmail.com>
2021-03-01 13:40:05 +1100

    spec out passwordless#login

--- spec/features/bootstrap_and_signup_spec.rb
+++ spec/features/bootstrap_and_signup_spec.rb
@@ -42,5 +42,39 @@ feature "Bootstrap and signup", js: true do
         page.text
       end.to include "We hope you will shortly be accepted in to our beta program"
     end
+
+    When "she signs out" do
+      page.find("nav .nav-link", text: "Sign out").click
+      wait_for do
+        find(".alert span").text
+      end.to eq "Signed out successfully."
+    end
+
+    And "signs in again" do
+      find("form#new_user").fill_in("Email", with: "melanie.perkins@canva.com")
+      find("form#new_user").find('input[type="submit"]').click
+    end
+
+    Then "she can use the form to sign in via email token" do
+      pending "make passwodless login work"
+      # HINT: create a passwordless sign in email with a reset_token = user.send(:set_reset_password_token)
+      #       and a passwodless#login function with
+      #         user = User.with_reset_password_token(params[:reset_token]
+      #         sign_in user
+      wait_for do
+        find(".alert span").text
+      end.to eq "A message with a login link has been sent to your email address."
+      open_email "melanie.perkins@canva.com"
+      current_email.click_link "LOGIN TO MY ACCOUNT"
+      wait_for do
+        find(".alert span").text
+      end.to eq "You have logged in via email link."
+      wait_for do
+        page.find_all("nav .nav-link").map(&:text)
+      end.to contain_exactly(
+        "melanie.perkins@canva.com",
+        "Sign out",
+      )
+    end
   end
 end
