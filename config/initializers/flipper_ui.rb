8489668e0b00588171213029b536b4d961bf8e7a
Michael Milewski <saramic@gmail.com>
2021-08-14 17:52:09 +1000

    [🐬] don't use descriptions for features
    breaks the UI if they don't exist :(

--- config/initializers/flipper_ui.rb
+++ config/initializers/flipper_ui.rb
@@ -2,10 +2,10 @@ require "flipper/ui"
 require "flipper/adapters/active_record"
 
 Flipper::UI.configure do |config|
-  config.descriptions_source = lambda { |keys|
-    # descriptions loaded from YAML file or database (postgres, mysql, etc)
-    # return has to be hash of {String key => String description}
-  }
+  # config.descriptions_source = lambda { |keys|
+  #   descriptions loaded from YAML file or database (postgres, mysql, etc)
+  #   return has to be hash of {String key => String description}
+  # }
 
   # Defaults to false. Set to true to show feature descriptions on the list
   # page as well as the view page.
