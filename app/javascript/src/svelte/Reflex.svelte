fab4b40156ce53dbf1175c3dc1e4f32f13a830e0
Chris Wade <chris@wadespace.net>
2021-09-09 07:32:13 +1000

    Initial Setup of Reflex
    Use tenary to display change in state

--- app/javascript/src/svelte/Reflex.svelte
+++ app/javascript/src/svelte/Reflex.svelte
@@ -1,8 +1,25 @@
 <script>
+  import { onMount } from "svelte";
+  import { getButton, pressButton } from "../services/button.js";
+
+  export let demoButtonId;
+
+  let ready = false;
+
+  const changeReflexState = () => {
+    pressButton({ buttonId: demoButtonId })
+      .then(() => getButton({ buttonId: demoButtonId }))
+      .then(() => (ready = !ready));
+  };
 </script>
 
-<button class="btn btn-primary" data-testid="button">button</button>
-<div data-testid="message">reflex test started</div>
+<button
+  on:click={changeReflexState}
+  class={"btn " + (ready ? "btn-danger" : "btn-primary")}
+  data-testid="button">button</button
+>
+<div data-testid="message">
+  {!ready ? "almost time to boggie" : "reflex test started"}
+</div>
 
-<style>
-</style>
+<style></style>
